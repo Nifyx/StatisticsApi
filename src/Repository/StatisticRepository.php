8b1ca6330b2712794b82ebd7441b160e94992e94
Antoine <antoine.29.p@gmail.com>
2019-01-25 20:30:55 +0100

    feat/developp
    GetPenByidAndByPeriod is working

--- src/Repository/StatisticRepository.php
+++ src/Repository/StatisticRepository.php
@@ -63,4 +63,160 @@ final class StatisticRepository implements StatisticRepositoryInterface
         $this->entityManager->remove($statistic);
         $this->entityManager->flush();
     }
+
+    /**
+     * @param String $idPen
+     * @param String $time_start
+     * @param String $time_end
+     * @return array
+     */
+    public function getPenByPeriod(String $idPen, String $time_start, String $time_end): array
+    {
+        $time_start_format = date('Y-m-d H:i:s',$time_start);
+        $time_end_format = date('Y-m-d H:i:s',$time_end);
+
+        $query = $this->entityManager->createQuery('SELECT s 
+            FROM App\Entity\Statistic\Statistic s 
+            WHERE s.pen = :idPen 
+            AND s.createdAt 
+            BETWEEN :time_start AND :time_end')
+            ->setParameter('idPen',$idPen)
+            ->setParameter('time_start',$time_start_format)
+            ->setParameter('time_end',$time_end_format);
+
+        return $query->getResult();
+    }
+
+    /**
+     * @param String $idPen
+     * @param String $time_start
+     * @param String $time_end
+     * @return int
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function getTotalViewsForPenByPeriod(String $idPen, String $time_start, String $time_end): int
+    {
+        $time_start_format = date('Y-m-d H:i:s',$time_start);
+        $time_end_format = date('Y-m-d H:i:s',$time_end);
+
+        $connection = $this->entityManager->getConnection();
+
+        $sql = 'SELECT COUNT(*) as nbView 
+                FROM STATISTIC s
+                WHERE s.pen_id = :idPen
+                AND s.created_at BETWEEN :time_start AND :time_end';
+
+        $stmt = $connection->prepare($sql);
+        $stmt->execute([
+            'idPen' => $idPen,
+            'time_start' => $time_start_format,
+            'time_end' => $time_end_format
+        ]);
+
+        $totalViews = $stmt->fetch()['nbView'];
+
+        return $totalViews;
+    }
+
+    /**
+     * @param String $idPen
+     * @param String $time_start
+     * @param String $time_end
+     * @return array
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function getTotalViewsByOrigin(String $idPen, String $time_start, String $time_end): array
+    {
+        $time_start_format = date('Y-m-d H:i:s',$time_start);
+        $time_end_format = date('Y-m-d H:i:s',$time_end);
+
+        $connection = $this->entityManager->getConnection();
+
+        $sql = 'SELECT COUNT(*) as nbView, origin 
+                FROM STATISTIC s
+                WHERE s.pen_id = :idPen
+                AND s.created_at BETWEEN :time_start AND :time_end
+                GROUP BY s.origin';
+
+        $stmt = $connection->prepare($sql);
+        $stmt->execute([
+            'idPen' => $idPen,
+            'time_start' => $time_start_format,
+            'time_end' => $time_end_format
+        ]);
+
+        $origin = $stmt->fetchAll();
+
+        return $origin;
+    }
+
+    /**
+     * @param String $idPen
+     * @param String $time_start
+     * @param String $time_end
+     * @return array
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function getStatsPerDay(String $idPen, String $time_start, String $time_end): array
+    {
+        $time_start_format = date('Y-m-d',$time_start);
+        $time_end_format = date('Y-m-d',$time_end);
+
+        $date_start = new \DateTime($time_start_format);
+        $date_end = new \DateTime($time_end_format);
+
+        $connection = $this->entityManager->getConnection();
+
+        $daysArray = array();
+
+        while($date_start < $date_end){
+            $sql = "SELECT COUNT(*) as nbView, origin 
+                FROM STATISTIC s
+                WHERE s.pen_id = :idPen
+                AND s.created_at > :date_start AND s.created_at < DATE_ADD(:date_start, INTERVAL 1 DAY)
+                GROUP BY s.origin";
+
+            $stmt = $connection->prepare($sql);
+
+            $stmt->execute([
+                'idPen' => $idPen,
+                'date_start' => $date_start->format('Y-m-d')
+            ]);
+
+            $originArray = $stmt->fetchAll();
+
+            $originCodePen = "";
+            $nbViewCodePen = 0;
+            foreach($originArray as $key => $value){
+                if($value['origin'] == "codepen"){
+                    $originCodePen = $originArray[$key]['origin'];
+                    $nbViewCodePen = $originArray[$key]['nbView'];
+                    unset($originArray[$key]);
+                }
+            }
+
+            $nbTotalViewsExternal = 0;
+            $arrayList = array();
+            foreach($originArray as $key => $value){
+                $nbTotalViewsExternal += $value['nbView'];
+                array_push($arrayList,['url' => $value['origin'], 'totalViews' => $value['nbView']]);
+            }
+
+            array_push($daysArray,[
+                'day' => $date_start->getTimestamp(),
+                'totalViewsDay' => $nbViewCodePen + $nbTotalViewsExternal,
+                'origins' => array(
+                    $originCodePen => ['totalViews' => $nbViewCodePen],
+                    'externals' => array(
+                        'totalViews' => $nbTotalViewsExternal,
+                        'list' => $arrayList
+                    )
+                )
+            ]);
+
+            date_add($date_start,new \DateInterval('P1D'));
+        }
+
+        return $daysArray;
+    }
 }
